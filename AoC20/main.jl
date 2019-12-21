f2de6ef29c87c31ba3f3a19fa14dc276984c4dc8
Adi Čaušević <adi.causevic7@gmail.com>
2019-12-21 23:48:44 +0100

    Day 20 part 1

--- AoC20/main.jl
+++ AoC20/main.jl
@@ -1,5 +1,7 @@
-function load_maze()
-    file = open("C:\\Projects\\AdventOfCode\\Solutions\\AoC20\\input.txt") do file
+using DataStructures
+
+function load_maze(path)
+    file = open(path) do file
         read(file, String)
     end
 
@@ -17,32 +19,106 @@ function load_maze()
     return maze, width, height
 end
 
+function print_maze(maze)
+    for line in maze
+        println(join(line, ' '))
+    end
+end
+
+mutable struct State
+    pos::Tuple{Int64, Int64}
+    path_length::Int64
+end
+
 function t1()
+    maze, width, height = load_maze("C:\\Projects\\AdventOfCode\\Solutions\\AoC20\\input.txt")
 
-    maze, width, height = load_maze()
+    start::Tuple{Int64, Int64}  = (0, 0)
+    finish::Tuple{Int64, Int64} = (0, 0)
 
-    start = (0, 0)
+    dx = [0, -1, 0, 1]
+    dy = [-1, 0, 1, 0]
 
-    for i in 2:height
-        for j in 2:width
-            
+    portals = Dict{Tuple{Int64, Int64}, Tuple{Int64, Int64}}()
+
+    #Sorted 2 characters, position of the mid char, position of the point
+    warps = Dict{String, Tuple{Tuple{Int64, Int64}, Tuple{Int64, Int64}}}()
+
+    for i in 2: height - 1, j in 2:width - 1
+        if 'A' <= maze[i][j] <= 'Z'
+            entry = maze[i][j]
+            other = '0'
+            point_pos = (0, 0)
+
+            for dir in 1:4
+                point_pos = j+ dx[dir], i + dy[dir]
+                val = maze[point_pos[2]][point_pos[1]]
+                if val == '.'
+                    opposite_dir = mod1(dir + 2, 4)
+                    other = maze[i + dy[opposite_dir]][j+ dx[opposite_dir]]
+                    break
+                end
+            end
+
+            if other != '0'
+                if other == entry == 'A'
+                    start = point_pos
+                    maze[i][j] = '#'
+                    continue
+                elseif other == entry == 'Z'
+                    finish = point_pos
+                    maze[i][j] = '#'
+                    continue
+                else
+                    tmp = [entry, other]
+                    sort!(tmp)
+                    key = join(tmp)
+                    if in(key, keys(warps))
+                        tmp = warps[key]
+                        maze[i][j] = '@'
+                        maze[tmp[1][2]][tmp[1][1]] = '@'
+
+                        portals[tmp[1]] = point_pos
+                        portals[(j, i)] = tmp[2]
+                    else
+                        warps[key] = (j, i), point_pos
+                    end
+                end
+            end
         end
     end
 
+    visited_states = Set{Tuple{Int64, Int64}}([start])
+    to_visit = PriorityQueue{State, Int64}([(State(start, 0), 0)])
 
+    while !isempty(to_visit)
+        current_state = dequeue!(to_visit)
 
+        if current_state.pos == finish
+            println("First = $(current_state.path_length)")
+        end
 
-    print(maze)
+        for direction in 1:4
+            new_pos = (current_state.pos[1] + dx[direction], current_state.pos[2] + dy[direction])
 
+            pos_value = maze[new_pos[2]][new_pos[1]]
+            if pos_value == '#'
+                continue
+            elseif pos_value == '@'
+                new_pos = portals[new_pos]
+            end
 
+            if in(new_pos, visited_states)
+                continue
+            end
 
+            to_visit[State(new_pos, current_state.path_length + 1)] = current_state.path_length + 1
+            push!(visited_states, new_pos)
+        end
+    end
 end
 
 function t2()
-    #Reading file
-    file = open("C:\\Projects\\AdventOfCode\\Solutions\\AoC20\\input.txt") do file
-        read(file, String)
-    end
 
 end
 
@@ -52,4 +128,4 @@ Second: 80284420
 """
 
 t1()
-#t2()
+t2()
